f4ed79b6447bd64b6d37aeb052a0471cffc52660
my-mail <ccbik@outlook.com>
2026-05-14 20:40:32 +0800

    Update public-api.js

--- mail-worker/src/api/public-api.js
+++ mail-worker/src/api/public-api.js
@@ -63,6 +63,4 @@ const messageHandler = async (c) => {
 	return c.json(mappedResult);
 };
 
-// --- 👇 终极双重保险：同时绑定两个路径 👇 ---
-app.get('/messages', messageHandler);      // 应对 https://nmail.us.ci/messages 这种访问
-app.get('/api/messages', messageHandler);  // 应对 https://nmail.us.ci/api/messages 这种访问
+
